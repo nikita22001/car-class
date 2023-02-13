a6b9dba3ff4e34b3844c6e5aea49d320ed805604
nikita22001 <118523343+nikita22001@users.noreply.github.com>
2023-02-13 15:13:50 +0200

    .

--- car1.java
+++ car1.java
@@ -2,8 +2,18 @@
 public class car1 {
 
 	public static void main(String[] args) {
-		Car car = new Car();
-		car.printData();
+		Car car1 = new Car();
+		car1.printData();
+		car1.accelerate();
+		car1.brake();
+		car1.printData();
+		car1.refuel(5);
+		
+		Car car2 = new Car("Fait","200",10,"Yellow");
+		car2.printData();
+		
+		Car car3 = new Car("Hyundai","i20",30,"Red");
+		car3.printData();
 	}
 }
 
@@ -13,12 +23,23 @@ class Car {
 	public String brand;
 	public String model;
 	public int amountofFuel;
+	public String color;
 
+	//Default Constructor
 	public Car() {
 		accelerate = true;
 		brand = "Kia";
 		model = "Rio";
-		amountofFuel = 2;
+		amountofFuel = 20;
+		color="Black";
+	}
+	
+	//Parameterized constructor
+	public Car (String brand, String model,int amountofFuel,String color) {
+		accelerate = true;
+		this.brand=brand;
+		this.model=model;
+		this.amountofFuel=amountofFuel;
 	}
 
 	public void brake() {
@@ -26,18 +47,27 @@ class Car {
 		System.out.println("Car is breaking");
 
 	}
-
+//accerlerate method 
 	public void accelerate() {
 		accelerate = true;
-		if (amountofFuel > 0) {
-			System.out.println("Car is accelerating");
-			amountofFuel--;
+		if (amountofFuel <= 0) {
+			accelerate=false;
 		}
 	}
+	
+	public void refuel(int amount)
+	{
+		System.out.println("Fuel on the tank: "+ amountofFuel);
+		System.out.println("Refuel: " +amount);
+		System.out.println("Fuel on the tank after tthe refuel: " + (amountofFuel+amount));
+	}
+	
+
 
 	public void printData() {
 		System.out.println("Brand:" + brand);
 		System.out.println("Model:" + model);
 		System.out.println("Fuel:" + amountofFuel);
+	
 	}
 }
